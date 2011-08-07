d8fe51919befc05f8502efb49d7d0ba7e02775e7
biern <biern.m@gmail.com>
2011-08-07 11:34:01 +0200

    rearranged 'about' page

--- static/js/about.js
+++ static/js/about.js
@@ -1,21 +1,28 @@
 YUI({
-    //Last Gallery Build of this module
     gallery: 'gallery-2011.03.11-23-49'
 }).use("node", "gallery-dialog", function(Y) {
     var dialog = new Y.Dialog({ draggable: false }),
         name = "contact",
         server = "marcinbiernat.pl",
-        m = name + "@" + server,
-        msgNode = Y.Node.create("<div>email: </div>");
+        mail = name + "@" + server,
+        content;
 
-    msgNode.appendChild("<a></a>").set("text", m).set(
-        "href", "mailto:" + m);
+    Y.on("contentready", function () {
+        // Get html from <noscript> node
+        content = Y.Node.create(
+            Y.one("noscript").get("text")).one(".about-info-content");
+        // Set friendlier email form
+        content.one("#about-email").setContent(
+            Y.Node.create("<a>" + mail + "</a>").
+                set("href", "mailto:" + mail));
+    }, "noscript");
 
-    Y.all(".js-hide").hide();
-    Y.all(".js-show").show();
-    Y.one("#about-email-button").on("click", function (e) {
-        e.preventDefault();
-        dialog.build("Contact info", msgNode).addCloseButton();
-        dialog.show();
+    Y.on("domready", function () {
+        // Show email button and bind it to dialog
+        Y.one("#about-email-button").show().on("click", function (e) {
+            e.preventDefault();
+            dialog.build("Contact info", content).addCloseButton();
+            dialog.show();
+        });
     });
 });
