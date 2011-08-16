3b574169057b9ea0559d1fc7869a824588e049e6
biern <biern.m@gmail.com>
2011-08-16 22:43:39 +0200

    changed language code to 'en'

--- biern/settings_default.py
+++ biern/settings_default.py
@@ -16,7 +16,7 @@ TIME_ZONE = 'Poland'
 
 # Language code for this installation. All choices can be found here:
 # http://www.i18nguy.com/unicode/language-identifiers.html
-LANGUAGE_CODE = 'pl'
+LANGUAGE_CODE = 'en'
 
 SITE_ID = 1
 
